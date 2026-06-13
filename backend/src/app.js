a3a4d85c55854903632382989a95c9a7ba157306
Aayush kandhwe <aayushkandhwe3689@gmail.com>
2026-06-13 12:17:22 +0530

    added /health check

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,33 @@ import { registerRoutes } from "./loader/routes.js";
 export const createApp = () => {
   const app = express();
 
-
   // middleware
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
   // register router
-  registerRoutes(app)
+  registerRoutes(app);
+
+  app.get("/", (rea, res) => {
+    res.status(200).json({
+      success: true,
+      message: "Welcome to the server",
+    });
+  });
+
+  app.get("/health", (req, res) => {
+    res.status(200).json({
+      success: true,
+      message: "Healthy",
+      uptime: process.uptime(),
+      date: new Date().toLocaleDateString("en-GB", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      }),
+      time: new Date().toLocaleTimeString("en-IN"),
+    });
+  });
 
   return app;
-}
+};
